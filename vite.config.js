f27775c937ba1738ffebc9a5df530dd0ffad13b8
teeboy024 <adedejit46@gmail.com>
2024-11-12 12:06:11 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'public', // or 'build', 'dist'
+    index: 'index.html',
+    chunkSizeWarningLimit: 1000, // Increase limit (optional)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Customize chunking logic
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
